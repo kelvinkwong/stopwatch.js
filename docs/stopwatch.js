4c787f148c01e6148ce9ce29737b35698a33d691
Kelvin Wong <kelvinwong@outlook.com.au>
2021-11-21 13:10:45 +1100

    limit display to s:ms

--- docs/stopwatch.js
+++ docs/stopwatch.js
@@ -61,14 +61,15 @@ function formatTime(time) {
     var h = m = s = ms = 0;
     var newTime = '';
 
-    h = Math.floor( time / (60 * 60 * 1000) );
-    time = time % (60 * 60 * 1000);
-    m = Math.floor( time / (60 * 1000) );
-    time = time % (60 * 1000);
+    // h = Math.floor( time / (60 * 60 * 1000) );
+    // time = time % (60 * 60 * 1000);
+    // m = Math.floor( time / (60 * 1000) );
+    // time = time % (60 * 1000);
     s = Math.floor( time / 1000 );
     ms = time % 1000;
 
-    newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+    // newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+    newTime = pad(s, 2) + ':' + pad(ms, 3);
     return newTime;
 }
 
